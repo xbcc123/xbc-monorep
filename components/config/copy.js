db293baf2bf3bb8645ad4143911fccbe60efd32d
yankangjie <287207951@qq.com>
2021-07-07 11:01:49 +0800

    feat: 增加拷贝静态文件

--- components/config/copy.js
+++ components/config/copy.js
@@ -2,22 +2,62 @@ const fs = require('fs-extra')
 const NodePath = require('path')
 
 const resolveFile = path => NodePath.resolve(__dirname, path)
-const styleFile = resolveFile("../src/styles")
-const esStyleFile = resolveFile("../es/styles")
-const libStyleFile = resolveFile("../lib/styles")
 
-fs.copy(styleFile, esStyleFile, err => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log('copy style to es success!')
-})
+class Task {
+  constructor() {
+    this.resolveFile = resolveFile
 
-fs.copy(styleFile, libStyleFile, err => {
-  if (err) {
-    console.error(err)
-    return
+    // 样式
+    this.styleFile = this.resolveFile("../src/styles")
+    this.esStyleFile = this.resolveFile("../es/styles")
+    this.libStyleFile = this.resolveFile("../lib/styles")
+
+    // 静态资源
+    this.assetsFile = this.resolveFile("../src/assets")
+    this.esAssetsFile = this.resolveFile("../es/assets")
+    this.libAssetsFile = this.resolveFile("../lib/assets")
+
+    this.copyStyle()
+    this.copyAssets()
   }
-  console.log('copy style to lib  success!')
-})
+
+  // 拷贝styles
+  copyStyle() {
+    fs.copy(this.styleFile, this.esStyleFile, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log('copy style to es success!')
+    })
+
+    fs.copy(this.styleFile, this.libStyleFile, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log('copy style to lib  success!')
+    })
+  }
+
+  // 拷贝assets
+  copyAssets() {
+    fs.copy(this.assetsFile, this.esAssetsFile, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log('copy assets to es success!')
+    })
+
+    fs.copy(this.assetsFile, this.libAssetsFile, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log('copy assets to lib  success!')
+    })
+  }
+}
+
+new Task()
