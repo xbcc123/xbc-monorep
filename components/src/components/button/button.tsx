091808b6d489d042e32838ed321d0f4a4eb88e89
yankangjie <287207951@qq.com>
2021-07-07 14:51:28 +0800

    feat: 增加less-watch

--- components/src/components/button/button.tsx
+++ components/src/components/button/button.tsx
@@ -3,17 +3,18 @@ import React, { FC,  PropsWithChildren } from 'react';
 import { StandardProps } from '../../types/common';
 
 export interface XButtonProps extends StandardProps {
+  size: string
 }
 
 const prefixCls = 'x-button';
 const XButton: FC<XButtonProps> = (props: PropsWithChildren<XButtonProps>) => {
-    const { className, children, ...restProps } = props;
+    const { className, size = 'small', children, ...restProps } = props;
 
-    const classNames = classnames(prefixCls, className);
+    const classNames = classnames(prefixCls, className, `${prefixCls}__block--${size}`);
 
     return (
         <div className={classNames} {...restProps} >
-            我是一个demo按钮
+            我是一个demo按钮!!!
         </div>
     );
 };
