acef16c9894dd97f62d21ee6db5a0156458beb90
yankangjie <287207951@qq.com>
2021-07-08 14:06:05 +0800

    feat: 增加antd

--- components/src/components/button/button.tsx
+++ components/src/components/button/button.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames';
 import React, { FC,  PropsWithChildren } from 'react';
+import { Button } from 'antd';
 import { StandardProps } from '../../types/common';
 
 export interface XButtonProps extends StandardProps {
@@ -12,9 +13,12 @@ export const XButton: FC<XButtonProps> = (props: PropsWithChildren<XButtonProps>
     const { className, children, ...restProps } = props;
     const classNames = classnames(prefixCls, className);
     return (
+      <>
         <div className={classNames} {...restProps} >
             我是一个demo按钮!!
         </div>
+        <Button>我是一个ant按钮</Button>
+      </>
     );
 };
 
